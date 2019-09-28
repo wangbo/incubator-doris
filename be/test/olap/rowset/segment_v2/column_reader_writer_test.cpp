42c252f85aa2304cc194b13391e15392ee92489c
wangbo <506340561@qq.com>
2019-09-28 20:47:42 +0800

    +1234

--- be/test/olap/rowset/segment_v2/column_reader_writer_test.cpp
+++ be/test/olap/rowset/segment_v2/column_reader_writer_test.cpp
@@ -113,8 +113,8 @@ void test_nullable_data(uint8_t* src_data, uint8_t* src_is_null, int num_rows, s
         ASSERT_TRUE(st.ok());
 
         std::cout << "typeinfo size pre" << std::endl;
-        size_t = field->type_info()->size();
-        std::cout << "typeinfo size 7__:" << size_t << std::endl;
+        size_t n = field->type_info()->size();
+        std::cout << "typeinfo size 7__:" << n << std::endl;
         // sequence read
         {
             st = iter->seek_to_first();
