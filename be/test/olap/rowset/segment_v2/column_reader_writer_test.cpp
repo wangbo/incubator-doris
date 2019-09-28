539af5c31ac67f47d1f5cdb9147abfd54ff1d5bb
wangbo <506340561@qq.com>
2019-09-28 21:05:42 +0800

    as23123

--- be/test/olap/rowset/segment_v2/column_reader_writer_test.cpp
+++ be/test/olap/rowset/segment_v2/column_reader_writer_test.cpp
@@ -93,7 +93,7 @@ void test_nullable_data(uint8_t* src_data, uint8_t* src_is_null, int num_rows, s
         wfile.reset();
     }
     std::cout << "typeinfo size 2:" << field->type_info()->size() << std::endl;
-    std::cout << "sizeof4 " << sizeof(field->type_info()->size()) << endl;
+    std::cout << "sizeof4 " << sizeof(field->type_info()->size()) << std::endl;
     // read and check
     {
         // read and check
@@ -115,8 +115,8 @@ void test_nullable_data(uint8_t* src_data, uint8_t* src_is_null, int num_rows, s
 
         std::cout << "typeinfo size pre" << std::endl;
         size_t n = field->type_info()->size();
-        std::cout << "sizeof " << sizeof(n) << endl;
-        std::cout << "sizeof1 " << sizeof(field->type_info()->size()) << endl;
+        std::cout << "sizeof " << sizeof(n) << std::endl;
+        std::cout << "sizeof1 " << sizeof(field->type_info()->size()) << std::endl;
         std::cout << "typeinfo size 7__:" << n << std::endl;
         // sequence read
         {
