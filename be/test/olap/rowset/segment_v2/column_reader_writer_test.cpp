6b961866eba581ae3f6c6a88dca6001a56511c05
wangbo <506340561@qq.com>
2019-09-28 21:04:04 +0800

    +21323

--- be/test/olap/rowset/segment_v2/column_reader_writer_test.cpp
+++ be/test/olap/rowset/segment_v2/column_reader_writer_test.cpp
@@ -93,6 +93,7 @@ void test_nullable_data(uint8_t* src_data, uint8_t* src_is_null, int num_rows, s
         wfile.reset();
     }
     std::cout << "typeinfo size 2:" << field->type_info()->size() << std::endl;
+    std::cout << "sizeof4 " << sizeof(field->type_info()->size()) << endl;
     // read and check
     {
         // read and check
@@ -114,6 +115,8 @@ void test_nullable_data(uint8_t* src_data, uint8_t* src_is_null, int num_rows, s
 
         std::cout << "typeinfo size pre" << std::endl;
         size_t n = field->type_info()->size();
+        std::cout << "sizeof " << sizeof(n) << endl;
+        std::cout << "sizeof1 " << sizeof(field->type_info()->size()) << endl;
         std::cout << "typeinfo size 7__:" << n << std::endl;
         // sequence read
         {
