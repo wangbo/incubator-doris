df0722d0759eae27404d01a48e64f9953f7629d3
wangbo <506340561@qq.com>
2019-09-29 10:35:43 +0800

    add null log

--- be/test/olap/rowset/segment_v2/column_reader_writer_test.cpp
+++ be/test/olap/rowset/segment_v2/column_reader_writer_test.cpp
@@ -115,6 +115,8 @@ void test_nullable_data(uint8_t* src_data, uint8_t* src_is_null, int num_rows, s
 
         std::cout << "typeinfo size pre" << std::endl;
         size_t n = field->type_info()->size();
+        bool ret = n == nullptr;
+        std::cout << "is null" << ret << std::endl;
         std::cout << "sizeof " << sizeof(n) << std::endl;
         std::cout << "sizeof1 " << sizeof(field->type_info()->size()) << std::endl;
         std::cout << "typeinfo size 7__:" << n << std::endl;
