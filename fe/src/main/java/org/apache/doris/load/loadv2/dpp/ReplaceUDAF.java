5d87a6456587f6e65ee335c0b5b9e500e4892c46
wangbo <506340561@qq.com>
2020-04-21 20:34:21 +0800

    fix codestyle

--- fe/src/main/java/org/apache/doris/load/loadv2/dpp/ReplaceUDAF.java
+++ fe/src/main/java/org/apache/doris/load/loadv2/dpp/ReplaceUDAF.java
@@ -25,7 +25,6 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
