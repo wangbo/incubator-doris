6d33117633116c0395d1c5d7e45c5616e9c37966
morningman <morningman@163.com>
2023-07-07 12:34:45 +0800

    2.0.1-beta

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -30,7 +30,7 @@ set -eo pipefail
 build_version_prefix="doris"
 build_version_major=2
 build_version_minor=0
-build_version_patch=0
+build_version_patch=1
 build_version_rc_version="beta-datalake"
 
 build_version="${build_version_prefix}-${build_version_major}.${build_version_minor}-${build_version_rc_version}"
