6c4760556d394df0cf638666c7b1743f066ef052
wangbo <506340561@qq.com>
2023-07-10 17:17:20 +0800

    sleep 1s

--- be/src/vec/exec/scan/scanner_scheduler.cpp
+++ be/src/vec/exec/scan/scanner_scheduler.cpp
@@ -430,6 +430,7 @@ void ScannerScheduler::_task_group_scanner_scan(ScannerScheduler* scheduler,
                 SCOPED_RAW_TIMER(&time_spent);
                 scan_task.scan_func();
             }
+            sleep(2);
             scan_queue->update_statistics(scan_task, time_spent);
         }
     }
