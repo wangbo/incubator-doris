ffdf9c445e2152f97b3042d9985babb057fd2f72
wangbo <506340561@qq.com>
2023-08-09 10:57:55 +0800

    fix reg test script

---
+++ regression-test/suites/workload_manager_p1/conf/mixed_query_test_conf.groovy
@@ -0,0 +1,22 @@
+global_conf.enable_test="true"
+global_conf.url="jdbc:mysql://10.16.10.8:9230/hits?useSSL=false"
+global_conf.username="root"
+global_conf.password=""
+global_conf.enable_pipe="true"
+global_conf.enable_group="true"
+
+ckbench_query.label="ckbench query"
+ckbench_query.dir="../query/ckbench_query/"
+ckbench_query.c="1"
+ckbench_query.i="1"
+ckbench_query.group="normal"
+ckbench_query.db="hits"
+
+tpch_query.label="tpch query"
+tpch_query.dir="../query/tpch_query/"
+tpch_query.c="1"
+tpch_query.i="1"
+tpch_query.group="test"
+tpch_query.db="tpch100"
+
+
