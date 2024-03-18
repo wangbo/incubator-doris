bde9e23274b6be200b0193869097cfcbad640560
wangbo <506340561@qq.com>
2024-03-18 14:50:47 +0800

    delete fe cpu limit

--- fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -34,7 +34,6 @@ import org.apache.doris.planner.GroupCommitBlockSink;
 import org.apache.doris.qe.VariableMgr.VarAttr;
 import org.apache.doris.thrift.TGroupCommitMode;
 import org.apache.doris.thrift.TQueryOptions;
-import org.apache.doris.thrift.TResourceLimit;
 import org.apache.doris.thrift.TRuntimeFilterType;
 
 import com.google.common.base.Joiner;
@@ -2652,10 +2651,6 @@ public class SessionVariable implements Serializable, Writable {
         return groupByAndHavingUseAliasFirst;
     }
 
-    public int getCpuResourceLimit() {
-        return cpuResourceLimit;
-    }
-
     public int getSendBatchParallelism() {
         return sendBatchParallelism;
     }
@@ -3046,12 +3041,6 @@ public class SessionVariable implements Serializable, Writable {
         tResult.setRuntimeFilterWaitTimeMs(runtimeFilterWaitTimeMs);
         tResult.setRuntimeFilterMaxInNum(runtimeFilterMaxInNum);
         tResult.setRuntimeFilterWaitInfinitely(runtimeFilterWaitInfinitely);
-
-        if (cpuResourceLimit > 0) {
-            TResourceLimit resourceLimit = new TResourceLimit();
-            resourceLimit.setCpuLimit(cpuResourceLimit);
-            tResult.setResourceLimit(resourceLimit);
-        }
 
         tResult.setEnableFunctionPushdown(enableFunctionPushdown);
         tResult.setEnableCommonExprPushdown(enableCommonExprPushdown);
