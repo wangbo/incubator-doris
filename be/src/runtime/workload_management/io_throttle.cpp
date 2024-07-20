adf8ff489cc2c3f82fe47a0cc979f8156bd89d50
wangbo <506340561@qq.com>
2024-07-20 16:32:20 +0800

    +log

--- be/src/runtime/workload_management/io_throttle.cpp
+++ be/src/runtime/workload_management/io_throttle.cpp
@@ -38,8 +38,8 @@ bool IOThrottle::acquire(int64_t block_timeout_ms, int64_t* wait_count) {
     int64_t current_time = GetCurrentTimeMicros();
     int64_t block_finish_time = current_time + block_timeout_ms * 1000;
     
-    // int64_t old_current_time = current_time;
-    // int64_t old_next_io_time = _next_io_time_micros;
+    int64_t old_current_time = current_time;
+    int64_t old_next_io_time = _next_io_time_micros;
 
     uint64_t wc = 0;
     while (current_time <= _next_io_time_micros) {
@@ -51,9 +51,9 @@ bool IOThrottle::acquire(int64_t block_timeout_ms, int64_t* wait_count) {
         // wc++;
         current_time = GetCurrentTimeMicros();
     }
-    // LOG(INFO) << "old current time=" << old_current_time << ", new cur time=" << current_time
-    //           << ", old nex time=" << old_next_io_time << ", new next time=" << _next_io_time_micros
-    //           << ",wait time=" << (current_time - old_current_time);
+    LOG(INFO) << "old current time=" << old_current_time << ", new cur time=" << current_time
+              << ", old nex time=" << old_next_io_time << ", new next time=" << _next_io_time_micros
+              << ",wait time=" << (current_time - old_current_time);
     wc++;
     *wait_count = wc;
     return true;
@@ -84,13 +84,13 @@ void IOThrottle::update_next_io_time(int64_t io_bytes) {
 
     if (current_time > _next_io_time_micros) {
         _next_io_time_micros = current_time;
-        // reset_cur_time = true;
+        reset_cur_time = true;
     }
     _next_io_time_micros += ret < 1 ? static_cast<int64_t>(0) : static_cast<int64_t>(ret);
-    // LOG(INFO) << "io bytes:" << io_bytes << ", _next_io_time_micros=" << _next_io_time_micros
-    //           << ", old next_time=" << old_ << ", cur time=" << current_time
-    //           << ", delta=" << (_next_io_time_micros - old_)
-    //           << ",reset time=" << int(reset_cur_time);
+    LOG(INFO) << "io bytes:" << io_bytes << ", _next_io_time_micros=" << _next_io_time_micros
+              << ", old next_time=" << old_ << ", cur time=" << current_time
+              << ", delta=" << (_next_io_time_micros - old_)
+              << ",reset time=" << int(reset_cur_time);
 }
 
 void IOThrottle::set_io_bytes_per_second(int64_t io_bytes_per_second) {
