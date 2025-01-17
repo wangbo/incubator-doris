c2e77841dcb63965768dab184cc3e27f4eee2861
wangbo <wangbo@selectdb.com>
2025-01-17 17:05:02 +0800

    Fix double-free for memtable's aggregation

--- be/src/olap/memtable.cpp
+++ be/src/olap/memtable.cpp
@@ -62,6 +62,7 @@ MemTable::MemTable(int64_t tablet_id, std::shared_ptr<TabletSchema> tablet_schem
     g_memtable_cnt << 1;
     _query_thread_context.init_unlocked();
     _arena = std::make_unique<vectorized::Arena>();
+    _agg_func_arena = std::make_unique<vectorized::Arena>();
     _vec_row_comparator = std::make_shared<RowInBlockComparator>(_tablet_schema);
     _num_columns = _tablet_schema->num_columns();
     if (partial_update_info != nullptr) {
@@ -166,6 +167,7 @@ MemTable::~MemTable() {
         // Arena has to be destroyed after agg state, because some agg state's memory may be
         // allocated in arena.
         _arena.reset();
+        _agg_func_arena.reset();
         _vec_row_comparator.reset();
         _row_in_blocks.clear();
         _agg_functions.clear();
@@ -262,7 +264,7 @@ void MemTable::_aggregate_two_row_in_block(vectorized::MutableBlock& mutable_blo
             auto* col_ptr = mutable_block.mutable_columns()[cid].get();
             _agg_functions[cid]->add(dst_row->agg_places(cid),
                                      const_cast<const doris::vectorized::IColumn**>(&col_ptr),
-                                     src_row->_row_pos, _arena.get());
+                                     src_row->_row_pos, _agg_func_arena.get());
         }
     } else {
         DCHECK(_skip_bitmap_col_idx != -1);
@@ -279,7 +281,7 @@ void MemTable::_aggregate_two_row_in_block(vectorized::MutableBlock& mutable_blo
             auto* col_ptr = mutable_block.mutable_columns()[cid].get();
             _agg_functions[cid]->add(dst_row->agg_places(cid),
                                      const_cast<const doris::vectorized::IColumn**>(&col_ptr),
-                                     src_row->_row_pos, _arena.get());
+                                     src_row->_row_pos, _agg_func_arena.get());
         }
     }
 }
@@ -443,7 +445,7 @@ void MemTable::_finalize_one_row(RowInBlock* row,
                 auto* agg_place = row->agg_places(i);
                 auto* col_ptr = _output_mutable_block.get_column_by_position(i).get();
                 function->add(agg_place, const_cast<const doris::vectorized::IColumn**>(&col_ptr),
-                              row_pos, _arena.get());
+                              row_pos, _agg_func_arena.get());
             }
         }
     } else {
@@ -481,7 +483,7 @@ void MemTable::_aggregate() {
             auto* data = prev_row->agg_places(cid);
             _agg_functions[cid]->create(data);
             _agg_functions[cid]->add(data, const_cast<const doris::vectorized::IColumn**>(&col_ptr),
-                                     prev_row->_row_pos, _arena.get());
+                                     prev_row->_row_pos, _agg_func_arena.get());
         }
     };
 
